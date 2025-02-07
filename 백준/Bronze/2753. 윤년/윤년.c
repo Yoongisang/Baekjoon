b3c928ef76f60d1c3156b18e6334eff779a51557
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-02-07 23:02:04 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int A;
+
+	scanf("%d", &A);
+
+	if (A % 4 == 0 && A % 100 !=0)
+	{
+		printf("1\n");
+
+	}
+
+	else if (A % 400 == 0)
+	{
+		printf("1\n");
+
+	}
+
+	else 
+	{
+		printf("0");
+
+	}
+	return 0;
+}
