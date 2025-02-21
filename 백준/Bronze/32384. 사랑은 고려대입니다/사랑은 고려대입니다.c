e6f4ee90067e5fb6527426f3054eda082003a82a
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-02-21 23:58:37 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() 
+{
+    int N;
+    
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("LoveisKoreaUniversity ");
+    }
+
+
+    return 0;
+}
+
