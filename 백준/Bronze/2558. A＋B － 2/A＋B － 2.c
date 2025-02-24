45a9e30d1ff2c9957351a68049ca390ae62bea46
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-02-25 01:57:48 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    printf("%d\n", A + B);
+    return 0;
+}
