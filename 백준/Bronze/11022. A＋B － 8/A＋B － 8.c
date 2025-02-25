63530afa317d7fb36de2e6670f564dab2db16194
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-02-26 01:08:13 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    int A, B;
+    for (int i = 0; i < T; i++)
+    {
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d + %d = %d\n", i + 1, A, B,  A + B);
+    }
+    return 0;
+}
