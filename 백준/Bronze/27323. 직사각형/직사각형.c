db6bf01922f69b26cc2b85f899036c148f8f17ea
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-03-08 04:04:08 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    int A;
+    int B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    printf("%d\n", A * B);
+    return 0;
+}
