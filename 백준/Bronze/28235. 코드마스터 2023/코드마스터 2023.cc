f40a09d5bfd718765d20083c1bb51d4d6f74a411
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-03-26 00:39:02 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	if (s == "SONGDO")
+	{
+		cout << "HIGHSCHOOL" << endl;
+	}
+	else if (s == "CODE")
+	{
+		cout << "MASTER" << endl;
+	}
+	else if (s == "2023")
+	{
+		cout << "0611" << endl;
+	}
+	else if (s == "ALGORITHM")
+	{
+		cout << "CONTEST" << endl;
+	}
+	return 0;
+}
