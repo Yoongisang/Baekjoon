20e7e4462fa361bfe94c64182a84bc3baf52ed15
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2025-09-18 01:45:06 +0900

    [Silver I] Title: 절댓값 힙, Time: 12 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,54 @@
+#include <iostream>     // 표준 입출력 사용
+#include <vector>       // vector 컨테이너 사용
+#include <string>       // string 클래스 사용
+#include <algorithm>    // min 함수 사용
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+
+using namespace std;
+
+struct compare
+{
+	bool operator()(int a, int b)
+	{
+		if (abs(a) == abs(b))
+			return a > b;
+		return abs(a) > abs(b);
+	}
+};
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+
+	priority_queue<int, vector<int>, compare> q;
+
+	for (int i = 0; i < n; i++)
+	{
+		int input = 0;
+		cin >> input;
+
+		if (input == 0 && q.empty())
+		{
+			cout << "0" << "\n";
+		}
+		else if (input == 0 && !q.empty())
+		{
+			cout << q.top() << "\n";
+			q.pop();
+		}
+		else
+		{
+			q.push(input);
+		}
+		
+	}
+	
+	return 0;
+}
