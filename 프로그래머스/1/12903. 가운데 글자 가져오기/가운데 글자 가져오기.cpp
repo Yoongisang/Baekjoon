b6c80d54f21afec0230f731444e76fc55ae4388e
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-15 15:38:18 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -4,16 +4,6 @@
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
     int l = s.size();
-    if (l % 2 == 0)
-    {
-        answer.push_back(s[l / 2 - 1]);
-        answer.push_back(s[l / 2]);
-    }
-    else
-    {
-        answer.push_back(s[l / 2]);
-    }
-    return answer;
+    return s.substr((l - 1) / 2, 2 - (l % 2));
 }
