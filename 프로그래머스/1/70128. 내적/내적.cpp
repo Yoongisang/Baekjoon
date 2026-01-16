89f152f17746440b8b130650e5e2e9715242ff31
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-16 11:24:30 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cpp
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -1,13 +1,10 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 int solution(vector<int> a, vector<int> b) {
-    int answer = 0;
-    for (int i = 0; i < a.size(); ++i)
-    {
-        answer += a[i] * b[i];
-    }
+    int answer = inner_product(a.begin(),a.end(),b.begin(),0);
     return answer;
 }
