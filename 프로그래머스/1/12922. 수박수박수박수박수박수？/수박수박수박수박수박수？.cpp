ccc7c50d7e8595d764cdb4051b07d3a8a9a1f174
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-16 11:12:07 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.11 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    for (int i = 1; i <= n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         if (i % 2 == 0)
         {
-            answer += "박";
+            answer += "수";
         }
         else
         {
-            answer += "수";
+            answer += "박";
         }
     }
     return answer;
