0e7c461f318689dff211570327c3f3de18df3c8f
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-21 20:00:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -5,23 +5,28 @@
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
     int count = 1;
     for (auto& c : s)
     {
-        if (count % 2 != 0)
-        {
-            answer.push_back(toupper(c));
-        }
-        else
-        {
-             answer.push_back(tolower(c));
-        }
-        count++;
         if (c == ' ')
         {
             count = 1;
         }
+        else
+        {
+            if (count % 2 != 0)
+            {
+                c = toupper(c);
+            }
+            else
+            {
+                c = tolower(c);
+            }
+            count++;
+        }
+  
+       
+   
     }
-    return answer;
+    return s;
 }
