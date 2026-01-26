210f13a8e9c70d7f89d4c9fb674621ff950b966d
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-26 18:15:46 +0900

    [level 1] Title: 시저 암호, Time: 0.08 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            if (s[i] + n > 'z')
+            {
+                s[i] = s[i] + n - 26;
+            }
+            else
+            {
+                s[i] += n;
+            }
+        }
+        else if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            if (s[i] + n > 'Z')
+            {
+                s[i] = s[i] + n - 26;
+            }
+            else
+            {
+                s[i] += n;
+            }
+        }
+    }
+    
+    return s;
+}
