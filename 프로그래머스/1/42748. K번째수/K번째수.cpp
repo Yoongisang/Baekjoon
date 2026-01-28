d383331c86b810e4eebf152b99b0025c7f77a6f1
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-28 12:26:27 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -6,18 +6,14 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
+    vector<int> temp;
     
     for (auto& it : commands)
     {
-        vector<int> temp;
-        for (int i = it[0] - 1; i <= it[1] -1; i++)
-        {
-            temp.emplace_back(array[i]);
-        }
-        
-        sort(temp.begin(), temp.end());
-        
-        answer.emplace_back(temp[it[2] - 1]);
+        temp = array;
+
+        sort(temp.begin() + it[0] -1, temp.begin() + it[1]);
+        answer.emplace_back(temp[it[0] + it[2] - 2]);
     }
     return answer;
 }
