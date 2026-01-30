27e7bb983f07c668a096e07a928c26a735446817
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-01-30 12:29:31 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.30 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    string temp;
+    
+    int l = food.size();
+    for (int i = 1; i < l; i++)
+    {
+        int count = food[i] / 2;
+        for (int j = 0; j < count; j++)
+        {
+            answer += to_string(i);
+        }
+    }
+    
+    temp.assign(answer.rbegin(), answer.rend());
+    answer += '0' + temp;
+    
+    return answer;
+}
