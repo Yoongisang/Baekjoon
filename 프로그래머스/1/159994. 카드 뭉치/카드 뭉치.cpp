54bbb92a55e704110f341f843aa2fafa5e477c16
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-02-04 20:44:05 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "Yes";
+    int idx = 0;
+    int idx2 = 0;
+
+    for (auto& g : goal)
+    {
+        if (cards1[idx] == g)
+        {
+            idx++;
+        }
+        else
+        {
+            if (cards2[idx2] == g)
+            {
+                idx2++;
+            }
+            else
+            {
+                return "No";
+            }
+        }
+    }
+    return answer;
+}
