b9f601219e07fd780b61e25e1e242bdd51c73d95
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-02-09 10:14:20 +0900

    [level 1] Title: 기사단원의 무기, Time: 70.59 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    
+    for (int i = 1; i <= number; i++)
+    {
+        int temp = 0;
+        // 4 1 2 4
+        // 6 1 2 3 6
+        if (i == 1)
+        {
+            answer++;
+            continue;
+        }
+        
+        for (int j = 1; j * j <= i; j++)
+        {
+            if (j * j != i && i % j == 0)
+            {
+                temp += 2;
+            }
+            else if (j * j == i)
+            {
+                temp += 1;
+            }
+        }
+        
+        if (temp <= limit)
+        {
+            answer += temp;
+        }
+        else
+        {
+            answer += power;
+        }
+    }
+    return answer;
+}
