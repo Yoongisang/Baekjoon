3637cb7229945ab28174affc3c166026a5e2f6c8
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-02-13 13:12:15 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = n - lost.size();
+    // 여벌을 가져왔는데 본인도 도난당해서 못빌려주는 인원 제외
+    for (auto& l : lost)
+    {
+        auto it = find(reserve.begin(), reserve.end(), l);
+        if (it != reserve.end())
+        {
+            answer++;
+            reserve.erase(it);
+            l = -1;
+        }
+    }
+    
+    sort(lost.begin(), lost.end());
+    sort(reserve.begin(), reserve.end());
+    
+    for (auto& l : lost)
+    {
+        // 도난당한 학생의 앞 번호 뒷 번호 학생이 여벌 체육복 있는지 확인
+        auto prev = find(reserve.begin(), reserve.end(), l - 1);
+        auto next = find(reserve.begin(), reserve.end(), l + 1);
+        // 앞번호 학생이 있다면 앞번호 부터 빌리기 
+        if (prev != reserve.end()) // 잃어버린 학생 앞 번호가 가지고 여벌이 있다면
+        {
+            answer++;
+            reserve.erase(prev);
+        }
+        else if (next != reserve.end()) // 앞번호 학생이 없으면 뒷 번호한테 빌리기
+        {
+            answer++;
+            reserve.erase(next);
+        }
+    }
+    return answer;
+}
