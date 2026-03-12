30ea0614fa9d9490dac5091d77d39ea1fd6d38ca
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-03-12 17:22:07 +0900

    [level 1] Title: 공원 산책, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cpp
@@ -0,0 +1,95 @@
+#include <string>
+#include <vector>
+#include<cstdlib> 
+
+using namespace std;
+
+vector<int> solution(vector<string> park, vector<string> routes) {
+    vector<int> answer;
+    int x, y;
+
+    for (int i = 0; i < park.size(); i++)
+    {
+        for (int j = 0; j < park[i].size(); j++)
+        {
+            if (park[i][j] == 'S')
+            {
+                x = j;
+                y = i;
+            }
+        }
+    }
+
+    for (const auto& r : routes)
+    {
+       int num = 0;
+       num = r[2] - '0';
+
+        if (r[0] == 'N')
+        {
+            int temp = y;
+            
+            for (int i = 0; i < num; i++)
+            {
+                temp--;
+                
+                if (temp < 0 || park[temp][x] == 'X')
+                {
+                    temp = y;
+                    break;
+                }
+            }
+            y = temp;
+        }
+        else if (r[0] == 'S')
+        {
+            int temp = y;
+            
+            for (int i = 0; i < num; i++)
+            {
+                temp++;
+                
+                if (temp >= park.size() || park[temp][x] == 'X')
+                {
+                    temp = y;
+                    break;
+                }
+            }
+            y = temp;
+        }
+        else if (r[0] == 'W')
+        {
+            int temp = x;
+            
+            for (int i = 0; i < num; i++)
+            {
+                temp--;
+                
+                if (temp < 0 || park[y][temp] == 'X')
+                {
+                    temp = x;
+                    break;
+                }
+            }
+            x = temp;
+        }
+        else if (r[0] == 'E')
+        {
+            int temp = x;
+            
+            for (int i = 0; i < num; i++)
+            {
+                temp++;
+                
+                if (temp >= park[i].size() || park[y][temp] == 'X')
+                {
+                    temp = x;
+                    break;
+                }
+            }
+            x = temp;
+        }
+    }
+    answer = {y, x};
+    return answer;
+}
