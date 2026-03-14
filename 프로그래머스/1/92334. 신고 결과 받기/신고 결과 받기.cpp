20abdd208376facd608bf6caf24704ee590ac338
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-03-15 02:51:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 214.48 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -10,28 +10,24 @@ using namespace std;
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer(id_list.size());
     // 신고자 : 신고리스트
-    unordered_map<string, int> idx;
+
     unordered_map<string, unordered_set<string>> report_list;
     unordered_map<string, int> report_result;
     // 신고 성립 최소 조건
     if (id_list.size() <= k)
         return vector<int>(id_list.size(), 0);
 
-    for (int i = 0; i < id_list.size(); i++)
-    {
-        idx[id_list[i]] = i;
-    }
-    
+    // 신고자 : 유저 저장
     for (const auto& re : report)
     {
         stringstream ss(re);
         string key, value;
         ss >> key >> value;
-        
+        // 자동 중복 제거
         if (report_list[key].insert(value).second)
             report_result[value]++;
     }
-    
+    // 신고조건 충족시 
     for (int i = 0; i < id_list.size(); i++)
     {
         for (const auto& reported : report_list[id_list[i]])
