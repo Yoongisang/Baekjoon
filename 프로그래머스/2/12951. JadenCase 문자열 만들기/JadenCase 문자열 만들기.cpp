d25f67ffb2e83b072d36146640d534aa888f6ed9
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-03-17 17:16:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    stringstream ss(s);
+    string token;
+    while (getline(ss, token, ' '))
+    {
+        if (!token.empty() && isalpha(token[0]))
+        {
+            token[0] = toupper(token[0]);
+            transform(token.begin() + 1, token.end(), token.begin() + 1, ::tolower);
+            answer += token;
+          
+        }
+        else if (!token.empty())
+        {
+            transform(token.begin(), token.end(), token.begin(), ::tolower);
+            answer += token;
+        }
+        else if (token.empty())
+        {
+            answer += ' ';
+            continue;
+        }
+  
+        answer += ' ';
+    }
+    
+    answer.pop_back();
+    if (s.back() == ' ' && answer.back() != ' ')
+    answer += ' ';
+    return answer;
+}
