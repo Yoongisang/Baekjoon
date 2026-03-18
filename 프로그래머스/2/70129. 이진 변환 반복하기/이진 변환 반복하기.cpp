ffe4d681273c6f302345979e8ea26346aa945afc
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-03-18 20:24:06 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.19 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer(2);
+    int n = s.size();
+    int one = count(s.begin(), s.end(), '1');
+    int zero = s.size() - one;
+    while (n > 1)
+    {
+        int one_temp = 0;
+        int zero_temp = 0;
+        answer[1] += zero;
+        while (one > 0)
+        {
+            if (one % 2 == 1)
+            {
+                one_temp++;
+            }
+            else
+            {
+                zero_temp++;
+            }
+            one /= 2;
+        }
+        
+        one = one_temp;
+        zero = zero_temp;
+        n = one + zero;
+        answer[0]++;
+   
+    }
+    return answer;
+}
