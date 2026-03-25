f14408edfce30ba3510f343827bc4b4fba544bf4
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-03-25 21:20:40 +0900

    [level 2] Title: 귤 고르기, Time: 33.12 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+
+using namespace std;
+
+int solution(int k, vector<int> tangerine) {
+    int answer = 0;
+    map<int, int> list;
+    
+    for (const auto& t : tangerine)
+    {
+        list[t]++;
+    }
+    
+    vector<pair<int, int>> num_list(list.begin(), list.end());
+    sort(num_list.begin(), num_list.end(), [](const auto& a, const auto& b) {
+        return a.second > b.second;
+    });
+    
+    for (const auto& l : num_list)
+    {
+        if (k <= 0)
+            break;
+        
+        k -= l.second;
+        answer++;
+    }
+    return answer;
+}
