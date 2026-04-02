db5676c5377020e8a8432d1c19afbb300148ca65
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-04-02 18:56:02 +0900

    [level 2] Title: n^2 배열 자르기, Time: 31.67 ms, Memory: 24.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -1,35 +1,14 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
-    // 1 2 3 4
-    // 2 2 3 4
-    // 3 3 3 4
-    // 4 4 4 4
-    // 1 2 3 4 | 5 6 7 8 | 9 10 11 12 | 13 14 15
-    // 1 2 3 4 | 2 2 3 4 | 3  3  3  4 |  4  4  4
-    // 1 2 3 4 | 1 2 3 4 | 1  2  3  4 |
-    // i = i % n == 0 ? n : i % n
-    for (long long i = left + 1; i <= right + 1; i ++)
+    for (long long i = left; i <= right; i ++)
     {
-        if (i % n == 0)
-        {
-            answer.emplace_back(n);
-        }
-        else
-        {
-            if (i % n < (i / n) + 1)
-            {
-                answer.emplace_back((i / n) + 1);
-            }
-            else
-            {
-                answer.emplace_back(i % n);
-            }
-        }
+        answer.emplace_back(max(i % n, i / n) + 1);
     }
     return answer;
 }
