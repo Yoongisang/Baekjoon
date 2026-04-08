f682db4edf02c3a37090ac69aa8f2bc06783fc43
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-04-08 17:02:29 +0900

    [level 2] Title: 할인 행사, Time: 11.35 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -7,29 +7,30 @@ using namespace std;
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
     int answer = 0;
     int N = discount.size() - 9;
-    vector<unordered_map<string, int>> list(N);
-
+    unordered_map<string, int> window;
+    for (int i = 0; i < 10; i++)
+    {
+        window[discount[i]]++;
+    }
+    
     for (int i = 0; i < N; i++)
     {
         bool want_num = true;
-        for (int j = i; j < i + 10; j++)
-        {
-            list[i][discount[j]]++;
-        }
-        
         for (int j = 0; j < want.size(); j++)
         {
-            string temp = want[j];
-            if (list[i][temp] < number[j])
+            if (window[want[j]] < number[j])
             {
                 want_num = false;
                 break;
             }
         }
-        
         if (want_num == true)
             answer++;
+        if (i + 10 < discount.size())
+        {
+            window[discount[i]]--;
+            window[discount[i + 10]]++;
+        }
     }
-    
     return answer;
 }
