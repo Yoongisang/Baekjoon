794c8515058f4ea9b9fccf535d7d7cfc7e2eb981
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-04-29 11:09:22 +0900

    [level 1] Title: 모의고사, Time: 0.30 ms, Memory: 3.97 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -7,38 +7,24 @@ using namespace std;
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
     vector<vector<int>> ans(4);
-    ans[1] = {1, 2, 3, 4, 5};
-    ans[2] = {2, 1, 2, 3, 2, 4, 2, 5};
-    ans[3] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    ans[1] = { 1, 2, 3, 4, 5 };
+    ans[2] = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    ans[3] = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     vector<int> score(4, 0);
     
     for (int i = 0; i < answers.size(); i++)
     {
-        if (answers[i] == ans[1][i % 5])
+        for (int j = 1; j < 4; j++)
         {
-            score[1]++;
-        }
-        
-        if (answers[i] == ans[2][i % 8])
-        {
-            score[2]++;
-        }
-        
-        if (answers[i] == ans[3][i % 10])
-        {
-            score[3]++;
+            if (answers[i] == ans[j][i % ans[j].size()])
+                score[j]++;
         }
     }
-    
     int max_num = *max_element(score.begin(), score.end());
-    
-    for (int i = 0; i < 4; i++)
+    for (int i = 1; i < 4; i++)
     {
         if (score[i] == max_num)
-        {
             answer.emplace_back(i);
-        }
     }
-    
     return answer;
 }
